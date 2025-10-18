86173bd7ede0d533831a3adcd83ff90c54d9461e
hasankhalifa2001-glitch <hasan.khalifa2001@gmail.com>
2025-10-18 16:11:23 +0300

    update

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,18 +1,19 @@
-import prisma from "@/utils/db";
-import { verifyToken } from "@/utils/verifyToken";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { UpdateArticleDto } from '@/utils/dtos';
+import prisma from '@/utils/db';
+import { verifyToken } from '@/utils/verifyToken';
 
 interface Props {
     params: { id: string }
 }
 
-/**
- *      @method GET
- *      @route http://localhost:3000/api/articles/:id
- *      @description Get Single Article by id
- *      @access public  
-*/
 
+/**
+ *  @method  GET
+ *  @route   ~/api/articles/:id
+ *  @desc    Get Single Article By Id
+ *  @access  public
+ */
 export async function GET(request: NextRequest, { params }: Props) {
     try {
         const article = await prisma.article.findUnique({
@@ -21,128 +22,106 @@ export async function GET(request: NextRequest, { params }: Props) {
                 comment: {
                     include: {
                         User: {
-                            select: { username: true }
+                            select: {
+                                username: true,
+                            }
                         }
                     },
                     orderBy: {
                         createdAt: 'desc'
                     }
                 }
-            },
-        })
+            }
+        });
+
         if (!article) {
-            return NextResponse.json(
-                { message: 'Article not found' },
-                { status: 400 }
-            )
+            return NextResponse.json({ message: 'article not found' }, { status: 404 });
         }
 
-        return NextResponse.json(article, { status: 200 })
-
+        return NextResponse.json(article, { status: 200 });
     } catch (error) {
         return NextResponse.json(
-            { message: "Internal Server Error" },
+            { message: "internal server error" },
             { status: 500 }
-        )
+        );
     }
 }
 
-/**
- *      @method PUT
- *      @route http://localhost:3000/api/articles/:id
- *      @description Update Article by id
- *      @access private  
-*/
 
+/**
+ *  @method  PUT
+ *  @route   ~/api/articles/:id
+ *  @desc    Update Article
+ *  @access  private (only admin can update article)
+ */
 export async function PUT(request: NextRequest, { params }: Props) {
     try {
-
-        const user = verifyToken(request)
-
-        if (user === null || user?.isAdmin === false) {
+        const user = verifyToken(request);
+        if (user === null || user.isAdmin === false) {
             return NextResponse.json(
-                { message: "Only admin" },
+                { message: 'only admin, access denied' },
                 { status: 403 }
-            );
-        }
-
-        const article = await prisma.article.findUnique({ where: { id: parseInt(params.id) } })
-        if (!article) {
-            return NextResponse.json(
-                { message: 'Article not found' },
-                { status: 400 }
             )
         }
 
-        const body = await request.json() as UpdateArticleDto
+        const article = await prisma.article.findUnique({
+            where: { id: parseInt(params.id) }
+        });
 
+        if (!article) {
+            return NextResponse.json({ message: 'article not found' }, { status: 404 });
+        }
+
+        const body = (await request.json()) as UpdateArticleDto;
         const updatedArticle = await prisma.article.update({
             where: { id: parseInt(params.id) },
             data: {
                 title: body.title,
                 description: body.description
             }
-        })
-        return NextResponse.json(
-            { message: "Updated article", updatedArticle },
-            { status: 200 }
-        )
+        });
 
+        return NextResponse.json(updatedArticle, { status: 200 });
     } catch (error) {
         return NextResponse.json(
-            { message: "Internal Server Error" },
+            { message: "internal server error" },
             { status: 500 }
-        )
+        );
     }
 }
 
 /**
- *      @method DELETE
- *      @route http://localhost:3000/api/articles/:id
- *      @description Delete Article by id
- *      @access public  
-*/
-
+ *  @method  DELETE
+ *  @route   ~/api/articles/:id
+ *  @desc    Delete Article
+ *  @access  private (only admin can delete article)
+ */
 export async function DELETE(request: NextRequest, { params }: Props) {
     try {
-
-        const user = verifyToken(request)
-
-        if (user === null || user?.isAdmin === false) {
+        const user = verifyToken(request);
+        if (user === null || user.isAdmin === false) {
             return NextResponse.json(
-                { message: "Only admin" },
+                { message: 'only admin, access denied' },
                 { status: 403 }
-            );
+            )
         }
 
         const article = await prisma.article.findUnique({
             where: { id: parseInt(params.id) },
             include: { comment: true }
-        })
+        });
         if (!article) {
-            return NextResponse.json(
-                { message: 'Article not found' },
-                { status: 400 }
-            )
+            return NextResponse.json({ message: 'article not found' }, { status: 404 });
         }
 
-        // delete the article
-        await prisma.article.delete({ where: { id: parseInt(params.id) } })
+        // deleting the article
+        await prisma.article.delete({ where: { id: parseInt(params.id) } });
 
-        // delete the comment that belongs to the article
-        const commentIds: number[] = article?.comment.map(comment => comment.id)
-
-        await prisma.comment.deleteMany({ where: { id: { in: commentIds } } })
-
-        return NextResponse.json(
-            { message: 'Article deleted successfully' },
-            { status: 200 }
-        )
-
+        return NextResponse.json({ message: 'article deleted' }, { status: 200 });
     } catch (error) {
         return NextResponse.json(
-            { message: "Internal Server Error" },
+            { message: "internal server error" },
             { status: 500 }
-        )
+        );
     }
 }
--- src/utils/dtos.ts
+++ src/utils/dtos.ts
@@ -1,36 +1,35 @@
-interface CreateArticleDto { // Data Transfer Object
-    title: string,
-    description: string
+export interface CreateArticleDto {
+    title: string;
+    description: string;
 }
 
-interface UpdateArticleDto { // Data Transfer Object
-    title?: string,
-    description?: string
+export interface UpdateArticleDto {
+    title?: string;
+    description?: string;
 }
 
-interface RegisterUserDto { // Data Transfer Object
-    username: string,
-    email: string,
-    password: string
+export interface RegisterUserDto {
+    username: string;
+    email: string;
+    password: string;
 }
 
-interface LoginUserDto { // Data Transfer Object
-    email: string,
-    password: string
+export interface LoginUserDto {
+    email: string;
+    password: string;
 }
 
-interface UpdateUserDto { // Data Transfer Object
-    username?: string,
-    email?: string,
-    password?: string,
+export interface UpdateUserDto {
+    username?: string;
+    email?: string;
+    password?: string;
 }
 
-interface CreateCommentDto { // Data Transfer Object
-    text: string,
-    articleId: number,
+export interface CreateCommentDto {
+    text: string;
+    articleId: number;
 }
 
-interface UpdateCommentDto { // Data Transfer Object
-    text: string,
+export interface UpdateCommentDto {
+    text: string;
 }
-
