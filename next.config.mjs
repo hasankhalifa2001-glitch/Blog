079754625f79cc6c349e2ea0a774001127fd9d15
hasankhalifa2001-glitch <hasan.khalifa2001@gmail.com>
2025-10-18 15:55:15 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'photo.com'
-    //         }
-    //     ]
-    // }
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // images: {
+//     //     remotePatterns: [
+//     //         {
+//     //             protocol: 'https',
+//     //             hostname: 'photo.com'
+//     //         }
+//     //     ]
+//     // }
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    experimental: {
+        appDir: true
+    }
+}
